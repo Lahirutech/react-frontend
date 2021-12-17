f384b3f3176530bc7c1870d5332a51964eeb0fc0
Lanka Rathnayake <LankaR@99x.io>
2021-12-17 23:33:27 +0530

    adding the nav links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
 export default function Navbar() {
+    const [click, setClick] = useState(false)
+    const [button, setButton] = useState(false)
     const closeMobileMenu = () => {
 
     }
+    const handleClick = () => {
+
+    }
+
     return (
         <>
             <nav className='navbar'>
@@ -16,13 +22,36 @@ export default function Navbar() {
                     <div className='menu-icon' onClick={handleClick}>
                         <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
                     </div>
-
+                    <ul>
+                        <li>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Resturants
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Leasure
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Spa
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                Sign up
+                            </Link>
+                        </li>
+                    </ul>
+                    {button && <Button buttonStyle='btn--outline'>SIGN UP</Button>}
                 </div>
-
             </nav>
-            <p>Lanka</p>
-
-
         </>
     )
 }
