18b11f65d24af125bc583640da84fc73c3680b6b
Lanka Rathnayake <LankaR@99x.io>
2021-12-19 01:23:01 +0530

    font change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -29,7 +29,7 @@ function Navbar() {
             <nav className='navbar'>
                 <div className='navbar-container'>
                     <Link to='/' className='navbar-logo' onClick={closeMobileMenu}>
-                        TRVL
+                        Ceylon Travel Styles
                         <i class='fab fa-typo3' />
                     </Link>
                     <div className='menu-icon' onClick={handleClick}>
