6e0896ecc7a2f06745905bce4094662560a82505
Lanka Rathnayake <LankaR@99x.io>
2021-12-19 00:29:16 +0530

    add styles

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
 import { Button } from './Button';
+import './Navbar.css';
+
 
 export default function Navbar() {
     const [click, setClick] = useState(false)
