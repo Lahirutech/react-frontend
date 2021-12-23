69e9d28b6113efc9ad7229d31710463ff4f6e203
Lanka Rathnayake <LankaR@99x.io>
2021-12-23 22:22:04 +0530

    adding the hero section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './components/pages/Home';
 
 
 function App() {
@@ -9,6 +10,9 @@ function App() {
     <div>
       <Router>
         <Navbar />
+        <Switch>
+          <Route index='/' component={Home} />
+        </Switch>
       </Router>
     </div>
   );
---
+++ src/components/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../../App.css'
+import HeroSection from '../HeroSection';
+
+export default function Home() {
+    return (
+        <div>
+            <HeroSection />
+        </div>
+    )
+}
